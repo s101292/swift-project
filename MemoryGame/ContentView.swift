04d092d0b8cf363a9ef2537b0af785cbb945e28c
Eduard <edlayweb@gmail.com>
2024-01-14 19:44:48 +0100

    update

--- MemoryGame/ContentView.swift
+++ MemoryGame/ContentView.swift
@@ -6,71 +6,71 @@ struct ContentView: View {
     @State private var lastScoreChange: (points: Int, cardId: String) = (0, "0")
     
     var body: some View {
-     VStack {
-        if viewModel.status == "player1_loose" || viewModel.status == "player2_loose" {
-            switch viewModel.status {
-            case "player1_loose":
-                Text("Player1 lost").font(.title2)
-            case "player2_loose":
-                Text("Player2 lost").font(.title2)
-            default:
-                EmptyView()
-            }
-        } else {
-            Text("Minefield 💣").font(.title2)
-            ScrollView {
-                cards.animation(.default, value: viewModel.cards)
-            }
-            Text("Let's find: " + viewModel.needToFind.content).font(.title2).padding(.bottom, 20)
-
-            HStack(spacing: 20) {
-                ScoreBox(score: viewModel.score1, playerName: "Player 1", color: Color.blue)
+        VStack {
+            if viewModel.status == "player1_loose" || viewModel.status == "player2_loose" {
                 switch viewModel.status {
-                case "player1":
-                    Text("Player 1, your turn! Good luck!").multilineTextAlignment(.center)
-                case "player2":
-                    Text("Player 2, your turn! Good luck!").multilineTextAlignment(.center)
-                case "remember":
-                    Text("Let's remember for 10sec...").multilineTextAlignment(.center)
+                case "player1_loose":
+                    Text("Player1 lost").font(.title2)
+                case "player2_loose":
+                    Text("Player2 lost").font(.title2)
                 default:
                     EmptyView()
                 }
-                ScoreBox(score: viewModel.score2, playerName: "Player 2", color: Color.green)
-            }
+            } else {
+                Text("Minefield 💣").font(.title2)
+                ScrollView {
+                    cards.animation(.default, value: viewModel.cards)
+                }
+                Text("Let's find: " + viewModel.needToFind.content).font(.title2).padding(.bottom, 20)
 
-            switch viewModel.status {
-            case "player1", "player2":
-                EmptyView()
-            case "round_end":
-                Button(action: {
-                    viewModel.hideAll()
-                }) {
-                    Text("NEXT ROUND")
-                        .font(.headline)
-                        .foregroundColor(.white)
-                        .padding()
-                        .frame(minWidth: 0, maxWidth: .infinity)
-                        .background(LinearGradient(gradient: Gradient(colors: [Color.blue, Color.purple]), startPoint: .leading, endPoint: .trailing))
-                        .cornerRadius(10)
-                        .padding(.horizontal, 20)
+                HStack(spacing: 20) {
+                    ScoreBox(score: viewModel.score1, playerName: "Player 1", color: Color.blue)
+                    switch viewModel.status {
+                    case "player1":
+                        Text("Player 1, your turn! Good luck!").multilineTextAlignment(.center)
+                    case "player2":
+                        Text("Player 2, your turn! Good luck!").multilineTextAlignment(.center)
+                    case "remember":
+                        Text("Let's remember for 10sec...").multilineTextAlignment(.center)
+                    default:
+                        EmptyView()
+                    }
+                    ScoreBox(score: viewModel.score2, playerName: "Player 2", color: Color.green)
                 }
-            default:
-                Button(action: {
-                    viewModel.hideAll()
-                }) {
-                    Text(viewModel.status == "remember" ? "REMEMBER" : "START GAME")
-                        .font(.headline)
-                        .foregroundColor(.white)
-                        .padding()
-                        .frame(minWidth: 0, maxWidth: .infinity)
-                        .background(LinearGradient(gradient: Gradient(colors: [Color.blue, Color.purple]), startPoint: .leading, endPoint: .trailing))
-                        .cornerRadius(10)
-                        .padding(20)
+
+                switch viewModel.status {
+                case "player1", "player2":
+                    EmptyView()
+                case "round_end":
+                    Button(action: {
+                        viewModel.hideAll()
+                    }) {
+                        Text("NEXT ROUND")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                            .padding()
+                            .frame(minWidth: 0, maxWidth: .infinity)
+                            .background(LinearGradient(gradient: Gradient(colors: [Color.blue, Color.purple]), startPoint: .leading, endPoint: .trailing))
+                            .cornerRadius(10)
+                            .padding(.horizontal, 20)
+                    }
+                default:
+                    Button(action: {
+                        viewModel.hideAll()
+                    }) {
+                        Text(viewModel.status == "remember" ? "REMEMBER" : "START GAME")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                            .padding()
+                            .frame(minWidth: 0, maxWidth: .infinity)
+                            .background(LinearGradient(gradient: Gradient(colors: [Color.blue, Color.purple]), startPoint: .leading, endPoint: .trailing))
+                            .cornerRadius(10)
+                            .padding(.top, 20)
+                    }
                 }
+                // themeButtonsDisplay
             }
-            // themeButtonsDisplay
         }
-    }.padding()
     }
     
     var cards : some View {
