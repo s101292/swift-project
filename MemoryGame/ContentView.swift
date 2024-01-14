992a5b6cd594618db16c0d58ceaebd083d4fbcef
Eduard <edlayweb@gmail.com>
2024-01-14 12:34:26 +0100

    update

--- MemoryGame/ContentView.swift
+++ MemoryGame/ContentView.swift
@@ -14,7 +14,7 @@ struct ContentView: View {
             ScrollView {
                 cards.animation(.default, value: viewModel.cards)
             }
-            Text("Let's find: ").font(.title3)
+            Text("Let's find: " + viewModel.needToFind.content).font(.title3)
             HStack {
                 Text("player1: \(viewModel.score1)")
                 Text("player2: \(viewModel.score2)")
