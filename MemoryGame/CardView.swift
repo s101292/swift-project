93fe9e401c1845a086a26882cd138740b0b62d2b
Eduard <edlayweb@gmail.com>
2024-01-14 20:34:20 +0100

    update

--- MemoryGame/CardView.swift
+++ MemoryGame/CardView.swift
@@ -13,7 +13,7 @@ struct CardView: View {
             .overlay(
                 Group{
                         base.fill(.white)
-                        .strokeBorder(card.isMatched ? Color.yellow : Color.blue, lineWidth: 3)
+                        base.strokeBorder(card.isMatched ? Color.yellow : Color.blue, lineWidth: 3)
                         Text(card.content)
                             .font(.system(size: 250))
                             .minimumScaleFactor(0.01)
--- MemoryGame/ContentView.swift
+++ MemoryGame/ContentView.swift
@@ -65,7 +65,7 @@ struct ContentView: View {
                         }
                 }
             }
-        }
+        }.padding()
     }
     
     var cards : some View {
